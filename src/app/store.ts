75096b355809b09a87fbacd7c79b7011c7745b13
Ilia Demahovsky <tjeker61@gmail.com>
2026-07-05 19:12:07 +0300

    Persist Redux store state to localStorage and initialize with preloaded state

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,20 @@
 import {configureStore} from "@reduxjs/toolkit";
 import user from "../features/user/userSlice";
 import token from "../features/token/tokenSlice";
+import type {UserProfile} from "../utils/types";
 
 export const store = configureStore(
     {
         reducer: {
             user, token
-        }
+        },
+        preloadedState: JSON.parse(localStorage.getItem('state') || '{}') as { token: string, user: UserProfile },
     }
 )
+
+store.subscribe(() => {
+    localStorage.setItem('state', JSON.stringify(store.getState()))
+})
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
